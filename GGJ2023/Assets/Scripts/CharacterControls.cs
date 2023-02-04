8280868920eac99a15aa7bb8d53a9543877caad0
Christos_Krilis <christos.krilis.dev@gmail.com>
2023-02-04 15:53:28 +0200

    Update CharacterControls.cs

--- GGJ2023/Assets/Scripts/CharacterControls.cs
+++ GGJ2023/Assets/Scripts/CharacterControls.cs
@@ -5,23 +5,43 @@ public class CharacterControls : MonoBehaviour
     
     private void Update()
     {
-        
-        CheckMouseClick();
+        CheckMouseInput();
     }
 
-
+    private void CheckMouseInput()
+    {
+        CheckLeftMouseClick();
+        CheckRightMouseClick();
+    }
     
-    private void CheckMouseClick()
+    private void CheckLeftMouseClick()
     {
         if (!Input.GetMouseButtonDown(0)) return;
-        
+        RayCastDetection(true);
+    }
+    
+    private void CheckRightMouseClick()
+    {
+        if (!Input.GetMouseButtonDown(1)) return;
+        RayCastDetection(false);
+    }
+    
+    private void RayCastDetection(bool isLeftClick)
+    {
         var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
         if (hit == null || hit.collider == null) return;
         
         var tile = hit.collider.GetComponent<Tile>();
-        if (tile)
+        if (tile == null) return;
+
+        if (isLeftClick)
         {
-            Debug.Log("You click a tile");
+            Debug.Log("Left : You click a tile");
+        }
+        else
+        {
+            Debug.Log("Right : safe digging");
         }
     }
+    
 }
