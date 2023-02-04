8075cb659d97391e1e72af1af0b0b93f41064ab0
Christos_Krilis <christos.krilis.dev@gmail.com>
2023-02-04 15:31:22 +0200

    Added Player Ray cast detection

--- GGJ2023/Assets/Scripts/CharacterControls.cs
+++ GGJ2023/Assets/Scripts/CharacterControls.cs
@@ -1,10 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class CharacterControls : MonoBehaviour
 {
-  
     
+    private void Update()
+    {
+        
+        CheckMouseClick();
+    }
+
+
+    
+    private void CheckMouseClick()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+        
+        var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit == null || hit.collider == null) return;
+        
+        var tile = hit.collider.GetComponent<Tile>();
+        if (tile)
+        {
+            Debug.Log("You click a tile");
+        }
+    }
 }
