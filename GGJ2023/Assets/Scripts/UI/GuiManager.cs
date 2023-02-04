b273793b17c3b5a337665a8314c274fa3cfb23bd
Christos_Krilis <christos.krilis.dev@gmail.com>
2023-02-04 18:18:56 +0200

    Added Gui Manager and GUI canvas prefab

---
+++ GGJ2023/Assets/Scripts/UI/GuiManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GuiManager : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI _scoreTxt;
+
+    public UnityAction<int> OnScoreChanged;
+
+    private void ChangeScore()
+    {
+        var x = OnScoreChanged;
+        _scoreTxt.text = x.ToString();
+    }
+
+    private void OnEnable()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _scoreTxt.text = "tetsa";
+    }
+
+}
