363bf0169c416f46b0da74ecb8e4a38b496915dd
Christos_Krilis <christos.krilis.dev@gmail.com>
2023-02-04 20:03:15 +0200

    Added animation on mouse click on tiles

--- GGJ2023/Assets/Scripts/Character/CharacterControls.cs
+++ GGJ2023/Assets/Scripts/Character/CharacterControls.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using DG.Tweening;
 using UnityEngine;
 
 public class CharacterControls : MonoBehaviour
 {
-    [SerializeField]private CharacterParams _characterParams;
-    [SerializeField]private Radar _radarPrefab;
+    [SerializeField] private CharacterParams _characterParams;
+    [SerializeField] private Radar _radarPrefab;
     [SerializeField] private ScoreManager _scoreManager;
     [SerializeField] private GuiManager _guiManager; //remove from here
 
@@ -21,38 +24,43 @@ public class CharacterControls : MonoBehaviour
 
     private void Update()
     {
+        // OnMouseOver();
         CheckMouseInput();
     }
 
     private void CheckMouseInput()
     {
+
         CheckLeftMouseClick();
         CheckRightMouseClick();
     }
-    
+
     private void CheckLeftMouseClick()
     {
         if (!Input.GetMouseButtonDown(0)) return;
         RayCastDetection(true);
     }
-    
+
     private void CheckRightMouseClick()
     {
         if (!Input.GetMouseButtonDown(1)) return;
         RayCastDetection(false);
     }
-    
+
     private void RayCastDetection(bool isLeftClick)
     {
         var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
         if (hit == null || hit.collider == null) return;
-        
+
         var tile = hit.collider.GetComponent<Tile>();
+
         if (tile == null) return;
-        
+
         if (isLeftClick)
         {
             tile.OnClick();
+            PopUpAnimation(tile.transform);
         }
         else
         {
@@ -65,14 +73,32 @@ public class CharacterControls : MonoBehaviour
         if (!_characterParams.CanSpawnRadar())
         {
             Debug.Log("No more radars available");
+
             return;
         }
-        
+
         var newRadar = Instantiate(_radarPrefab);
         _characterParams.IncreaseRadarCounter();
         var tilesList = GameController.Instance.mapController.ReturnTiles((int)tile.transform.position.x, (int)tile.transform.position.y);
         newRadar.SetTilesList(tilesList);
         tile.PlaceObject(newRadar.gameObject);
     }
+
+#region Animations
+
+    private void PopUpAnimation(Transform objTrasform) //todo : bug on multi clicks
+    {
+        var scale = objTrasform.transform.localScale;
+
+        objTrasform.transform.localScale = scale;
+        objTrasform.transform.DOKill();
+
+        objTrasform.transform.DOScale(new Vector3(1.1f, 1.1f, 1), 0.25f).SetEase(Ease.OutFlash).OnComplete(() =>
+        {
+            objTrasform.transform.DOScale(scale, 0.25f);
+        });
+    }
+
+#endregion
 
 }
