bb039fbe53cc38107db654ddebee59432bcb2c9c
Christos_Krilis <christos.krilis.dev@gmail.com>
2023-02-05 14:48:25 +0200

    added radar sprites and animations

--- GGJ2023/Assets/Scripts/Character/Radar.cs
+++ GGJ2023/Assets/Scripts/Character/Radar.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Enums;
 using UnityEngine;
 
@@ -6,11 +8,34 @@ public class Radar : MonoBehaviour
 {
 
     [SerializeField] private SpriteRenderer _areaSpriteRender;
-    
-    
+    [SerializeField] private GameObject _graphics;
+    [SerializeField] private SpriteRenderer _light;
+    [SerializeField] private Color _onDetect, _onEmpty;
+    private Color _color;
+    private bool _isReady = false;
+
     private void OnEnable()
     {
         GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public void Init(GameObject pinnedTile, List<GameObject> tiles)
+    {
+        transform.SetParent(pinnedTile.transform);
+        transform.localPosition = Vector3.zero;
+
+
+        _isReady = true;
+        SetTilesList(tiles);
+    }
+
+    
+    private void Update()
+    {
+        if(!_isReady) return;
+        
+        var lerpedColor = Color.Lerp(Color.white, _color, Mathf.PingPong(Time.time, 1));
+        _light.color = lerpedColor;
     }
 
     public void SetTilesList(List<GameObject> tiles)
@@ -21,6 +46,7 @@ public class Radar : MonoBehaviour
 
             if (tileObj.TileType == TileType.Root)
             {
+                _color = _onDetect;
                 //todo : score function here or something
                 //todo: play sfx
             }
